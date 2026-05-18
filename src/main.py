9da168f80970b8d8980685d83cdb834e22bc7652
Murilo-Barbiere <murilobarbiere@gmail.com>
2026-05-18 12:40:42 -0400

    data base e gammaAgente

--- src/main.py
+++ src/main.py
@@ -1,6 +1,12 @@
-import llm 
+from sqlalchemy import engine
+
+import llm.GammaAgente as GammaAgente 
+from database.models import Base
+
+Base.metadata.create_all(bind=engine)
+print("Banco criado.")
 
 query = input("pergunta pra llm: ")
 
-resposta = llm.perguntar_llm(query)
+resposta = GammaAgente.perguntar_llm(query)
 print(resposta) 
