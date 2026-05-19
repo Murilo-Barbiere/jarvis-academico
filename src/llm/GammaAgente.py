747ec57b96a11f2998c7c21c870525070f92b2e9
Murilo-Barbiere <murilobarbiere@gmail.com>
2026-05-19 12:40:06 -0400

    rag

--- src/llm/GammaAgente.py
+++ src/llm/GammaAgente.py
@@ -9,14 +9,30 @@ client = OpenAI(
     api_key=os.getenv("API_KEY")
 )
 
-def perguntar_llm(mensagem):
+def perguntar_llm(mensagem: str, contexto: str):
+
+    prompt = f"""
+Use o contexto abaixo para responder a pergunta.
+
+CONTEXTO:
+{contexto}
+
+PERGUNTA:
+{mensagem}
+"""
+
     try:
         resposta = client.chat.completions.create(
             model=os.getenv("MODEL"),
-            messages=[{'role': 'user', 'content': mensagem}],
+            messages=[
+                {
+                    'role': 'user',
+                    'content': prompt
+                }
+            ],
         )
-        
+
         return resposta.choices[0].message.content
-    
+
     except Exception as e:
         return f"Erro ao consultar LLM: {str(e)}"
--- src/main.py
+++ src/main.py
@@ -1,12 +1,68 @@
-from sqlalchemy import engine
+#imports------------
+import sys
 
-import llm.GammaAgente as GammaAgente 
-from database.models import Base
+import llm.GammaAgente as agente
 
-Base.metadata.create_all(bind=engine)
-print("Banco criado.")
+from rag.rag import (
+    MODEL_NAME,
+    PDF_PATH,
+    VetorStore,
+    gerar_contexto,
+    ler_pdfs,
+    preparar_documentos
+)
 
-query = input("pergunta pra llm: ")
+#imports------------
 
-resposta = GammaAgente.perguntar_llm(query)
-print(resposta) 
+
+def main():
+    # CARREGA PDFs
+    print("Carregando PDFs...\n")
+
+    documentos = ler_pdfs(PDF_PATH)
+
+    if not documentos:
+        print("Nenhum documento encontrado.")
+        sys.exit(1)
+
+    # CHUNKS
+    print("\nCriando chunks...\n")
+
+    chunks, metadados = preparar_documentos(documentos)
+
+    print(f"Total de chunks: {len(chunks)}\n")
+
+    # INDEXAÇÃO
+    vetor_store = VetorStore(MODEL_NAME)
+
+    vetor_store.adicionar_documentos(
+        chunks,
+        metadados
+    )
+
+    # LOOP PRINCIPAL
+    while True:
+
+        query = input("\nPergunta: ")
+
+        if query.lower() == "sair":
+            break
+
+        # BUSCA NO RAG
+        resultados = vetor_store.buscar(query)
+
+        # GERA CONTEXTO
+        contexto = gerar_contexto(resultados)
+
+        # CHAMA LLM
+        resposta = agente.perguntar_llm(
+            query,
+            contexto
+        )
+
+        print("\n================ RESPOSTA ================\n")
+        print(resposta)
+
+
+if __name__ == "__main__":
+    main()
