54219cc6eecf994e265357f3c02111b0000c1495
Joao <joaoeifler2004@gmail.com>
2026-05-23 14:31:19 -0400

    Mudança da pasta do db

--- src/config/setting.py
+++ src/config/setting.py
@@ -1,7 +1,7 @@
 import os
 
 PDF_PATH = os.getenv("PDF_PATH", "data")
-DB_PATH = os.getenv("DB_PATH", "data/agenda.db")
+DB_PATH = os.getenv("DB_PATH", "db/agenda.db")
 
 CHUNK_SIZE = 500
 CHUNK_OVERLAP = 100
